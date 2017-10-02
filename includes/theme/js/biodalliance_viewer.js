ff78b1797aaf97df389b5a7c5ce2d301764e6cd7
bradfordcondon <bradford.condon@gmail.com>
2017-10-02 16:53:18 -0400

    display default embed on organism

--- includes/theme/js/biodalliance_viewer.js
+++ includes/theme/js/biodalliance_viewer.js
@@ -1,32 +1,43 @@
-new Browser({
-    chr:          '22',
-    viewStart:    30700000,
-    viewEnd:      30900000,
+console.log("document ready");
 
-    coordSystem: {
-        speciesName: 'Human',
-        taxon: 9606,
-        auth: 'GRCh',
-        version: '37',
-        ucscName: 'hg19'
-    },
+(function($) {
+    Drupal.behaviors.tripal_daliance = {
+        attach: function() {
+            $(function(){
 
-    sources:     [{name:                 'Genome',
-        twoBitURI:            'http://www.biodalliance.org/datasets/hg19.2bit',
-        tier_type:            'sequence'},
-        {name:                 'Genes',
-            desc:                 'Gene structures from GENCODE 19',
-            bwgURI:               'http://www.biodalliance.org/datasets/gencode.bb',
-            stylesheet_uri:       'http://www.biodalliance.org/stylesheets/gencode.xml',
-            collapseSuperGroups:  true,
-            trixURI:              'http://www.biodalliance.org/datasets/geneIndex.ix'},
-        {name:                 'Repeats',
-            desc:                 'Repeat annotation from Ensembl',
-            bwgURI:               'http://www.biodalliance.org/datasets/repeats.bb',
-            stylesheet_uri:       'http://www.biodalliance.org/stylesheets/bb-repeats.xml'},
-        {name:                 'Conservation',
-            desc:                 'Conservation',
-            bwgURI:               'http://www.biodalliance.org/datasets/phastCons46way.bw',
-            noDownsample:         true}],
+                new Browser({
+                    chr:          '22',
+                    viewStart:    30700000,
+                    viewEnd:      30900000,
 
-});
+                    coordSystem: {
+                        speciesName: 'Human',
+                        taxon: 9606,
+                        auth: 'GRCh',
+                        version: '37',
+                        ucscName: 'hg19'
+                    },
+
+                    sources:     [{name:                 'Genome',
+                        twoBitURI:            'http://www.biodalliance.org/datasets/hg19.2bit',
+                        tier_type:            'sequence'},
+                        {name:                 'Genes',
+                            desc:                 'Gene structures from GENCODE 19',
+                            bwgURI:               'http://www.biodalliance.org/datasets/gencode.bb',
+                            stylesheet_uri:       'http://www.biodalliance.org/stylesheets/gencode.xml',
+                            collapseSuperGroups:  true,
+                            trixURI:              'http://www.biodalliance.org/datasets/geneIndex.ix'},
+                        {name:                 'Repeats',
+                            desc:                 'Repeat annotation from Ensembl',
+                            bwgURI:               'http://www.biodalliance.org/datasets/repeats.bb',
+                            stylesheet_uri:       'http://www.biodalliance.org/stylesheets/bb-repeats.xml'},
+                        {name:                 'Conservation',
+                            desc:                 'Conservation',
+                            bwgURI:               'http://www.biodalliance.org/datasets/phastCons46way.bw',
+                            noDownsample:         true}],
+
+                });
+            })
+        }
+    }
+})(jQuery)
