eb83f882ce4c6e4f82630226ebe250e4b97466ab
bradfordcondon <bradford.condon@gmail.com>
2017-10-03 13:26:07 -0400

    checkpoint

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 Module under development for Biodalliance genome viewer and comparative genome veiwer (synteny).
 
+
+## Module goals
+
+The ultimate goal for this module is to provide *embedded* genome views.  This would mean a viewer...
+
+* On the organism pane
+* For each individual feature, centered on that feature
+
+In addition, it should take advantage of the synteny display.
+
+Before we develop Tripal administration tools, first we want to make sure that this project will be worth implementing.
--- includes/theme/js/biodalliance_viewer.js
+++ includes/theme/js/biodalliance_viewer.js
@@ -1,40 +1,47 @@
-console.log("document ready");
-
 (function($) {
-    Drupal.behaviors.tripal_daliance = {
-        attach: function() {
+
+    Drupal.behaviors.tripal_biodalliance = {
+        attach: function(context, settings) {
             $(function(){
+                console.log(settings);
+
+                var $species_name =   settings.tripal_biodalliance.genome_name;
 
                 new Browser({
-                    chr:          '22',
-                    viewStart:    30700000,
-                    viewEnd:      30900000,
+                    chr:          'Scaffold00001',
+                    viewStart:    01,
+                    viewEnd:      1000000,
 
                     coordSystem: {
-                        speciesName: 'Human',
-                        taxon: 9606,
-                        auth: 'GRCh',
-                        version: '37',
-                        ucscName: 'hg19'
+                        speciesName: $species_name,
+                        //taxon: 9606,
+                       // auth: 'GRCh',
+                       // version: '37',
+                      //  ucscName: 'hg19'
                     },
 
-                    sources:     [{name:                 'Genome',
-                        twoBitURI:            'http://www.biodalliance.org/datasets/hg19.2bit',
-                        tier_type:            'sequence'},
-                        {name:                 'Genes',
-                            desc:                 'Gene structures from GENCODE 19',
-                            bwgURI:               'http://www.biodalliance.org/datasets/gencode.bb',
-                            stylesheet_uri:       'http://www.biodalliance.org/stylesheets/gencode.xml',
-                            collapseSuperGroups:  true,
-                            trixURI:              'http://www.biodalliance.org/datasets/geneIndex.ix'},
-                        {name:                 'Repeats',
-                            desc:                 'Repeat annotation from Ensembl',
-                            bwgURI:               'http://www.biodalliance.org/datasets/repeats.bb',
-                            stylesheet_uri:       'http://www.biodalliance.org/stylesheets/bb-repeats.xml'},
-                        {name:                 'Conservation',
-                            desc:                 'Conservation',
-                            bwgURI:               'http://www.biodalliance.org/datasets/phastCons46way.bw',
-                            noDownsample:         true}],
+                    //https://hardwoodgenomics.org/tools/jbrowse/index.html?data=chinese_chestnut&loc=scaffold00001%3A171733..257600&tracks=DNA&highlight=
+                sources: [{name: 'Genome',
+                    jbURI: 'http://jbrowse.app',
+                    jbQuery: 'data=sample_data'},
+                    ]
+                    // sources:     [{name:                 'Genome',
+                    //     twoBitURI:            'http://www.biodalliance.org/datasets/hg19.2bit',
+                    //     tier_type:            'sequence'},
+                    //     {name:                 'Genes',
+                    //         desc:                 'Gene structures from GENCODE 19',
+                    //         bwgURI:               'http://www.biodalliance.org/datasets/gencode.bb',
+                    //         stylesheet_uri:       'http://www.biodalliance.org/stylesheets/gencode.xml',
+                    //         collapseSuperGroups:  true,
+                    //         trixURI:              'http://www.biodalliance.org/datasets/geneIndex.ix'},
+                    //     {name:                 'Repeats',
+                    //         desc:                 'Repeat annotation from Ensembl',
+                    //         bwgURI:               'http://www.biodalliance.org/datasets/repeats.bb',
+                    //         stylesheet_uri:       'http://www.biodalliance.org/stylesheets/bb-repeats.xml'},
+                    //     {name:                 'Conservation',
+                    //         desc:                 'Conservation',
+                    //         bwgURI:               'http://www.biodalliance.org/datasets/phastCons46way.bw',
+                    //         noDownsample:         true}],
 
                 });
             })
