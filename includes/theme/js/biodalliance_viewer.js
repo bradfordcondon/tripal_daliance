aeb93483e80daa288ae702d36df3510c7e173988
bradfordcondon <bradford.condon@gmail.com>
2017-10-05 17:19:23 -0400

    experiment reading 2bit fiel instead

--- includes/theme/js/biodalliance_viewer.js
+++ includes/theme/js/biodalliance_viewer.js
@@ -6,43 +6,26 @@
                 console.log(settings);
 
                 var $species_name =   settings.tripal_biodalliance.genome_name;
+                var $reference_blob =   settings.tripal_biodalliance.blob;
 
                 new Browser({
-                    chr:          'Scaffold00001',
-                    viewStart:    01,
-                    viewEnd:      1000000,
+                    chr:          'scaffold1 0.0',
+                    viewStart:    0,
+                    viewEnd:      100,
 
                     coordSystem: {
                         speciesName: $species_name,
-                        //taxon: 9606,
-                       // auth: 'GRCh',
-                       // version: '37',
-                      //  ucscName: 'hg19'
+                        taxon: 9606,
+                        auth: 'GRCh',
+                        version: '37',
+                        ucscName: 'hg19'
                     },
 
-                    //https://hardwoodgenomics.org/tools/jbrowse/index.html?data=chinese_chestnut&loc=scaffold00001%3A171733..257600&tracks=DNA&highlight=
-                sources: [{name: 'Genome',
-                    jbURI: 'http://jbrowse.app',
-                    jbQuery: 'data=sample_data'},
-                    ]
-                    // sources:     [{name:                 'Genome',
-                    //     twoBitURI:            'http://www.biodalliance.org/datasets/hg19.2bit',
-                    //     tier_type:            'sequence'},
-                    //     {name:                 'Genes',
-                    //         desc:                 'Gene structures from GENCODE 19',
-                    //         bwgURI:               'http://www.biodalliance.org/datasets/gencode.bb',
-                    //         stylesheet_uri:       'http://www.biodalliance.org/stylesheets/gencode.xml',
-                    //         collapseSuperGroups:  true,
-                    //         trixURI:              'http://www.biodalliance.org/datasets/geneIndex.ix'},
-                    //     {name:                 'Repeats',
-                    //         desc:                 'Repeat annotation from Ensembl',
-                    //         bwgURI:               'http://www.biodalliance.org/datasets/repeats.bb',
-                    //         stylesheet_uri:       'http://www.biodalliance.org/stylesheets/bb-repeats.xml'},
-                    //     {name:                 'Conservation',
-                    //         desc:                 'Conservation',
-                    //         bwgURI:               'http://www.biodalliance.org/datasets/phastCons46way.bw',
-                    //         noDownsample:         true}],
+             //   /Users/chet/UTK/homesteads/localDrupal/sites/default/files/biodalliance
 
+                    sources:     [{name:                 'Genome',
+                        twoBitBlob:           $reference_blob,
+                        tier_type:            'sequence'}]
                 });
             })
         }
