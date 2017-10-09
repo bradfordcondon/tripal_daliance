736101b935d231401ef11d76e9eb3fd203a289c3
bradfordcondon <bradford.condon@gmail.com>
2017-10-09 11:04:51 -0400

    closerstill

--- includes/theme/js/biodalliance_viewer.js
+++ includes/theme/js/biodalliance_viewer.js
@@ -1,31 +1,42 @@
-(function($) {
+(function ($) {
 
     Drupal.behaviors.tripal_biodalliance = {
-        attach: function(context, settings) {
-            $(function(){
+        attach: function (context, settings) {
+            $(function () {
 
-                var species_name =   settings.tripal_biodalliance.species_name;
-              //  var $reference_blob =   settings.tripal_biodalliance.blob;
+                var species_name = settings.tripal_biodalliance.species_name;
+                //  var $reference_blob =   settings.tripal_biodalliance.blob;
                 var ref_path = settings.tripal_biodalliance.path;
-                var ref_blob = new File([""],ref_path);
 
-                new Browser({
-                    chr:          'scaffold1 0.0',
-                    viewStart:    0,
-                    viewEnd:      100,
+                var oReq = new XMLHttpRequest();
+                oReq.open("GET", ref_path, true);
+                oReq.responseType = "blob";
+                oReq.onload = function (oEvent) {
+                    var ref_blob = oReq.response;
+                    new Browser({
+                        chr: 'scaffold1 0.0',
+                        viewStart: 0,
+                        viewEnd: 100,
 
-                    coordSystem: {
-                        speciesName: species_name,
-                        taxon: 9606,
-                        auth: 'GRCh',
-                        version: '37',
-                        ucscName: 'hg19'
-                    },
+                        coordSystem: {
+                            speciesName: species_name,
+                            taxon: 9606,
+                            auth: 'GRCh',
+                            version: '37',
+                            ucscName: 'hg19'
+                        },
 
-                    sources:     [{name:                 'Genome',
-                        twoBitBlob:           ref_blob,
-                        tier_type:            'sequence'}]
-                });
+                        sources: [{
+                            name: 'Genome',
+                            twoBitBlob: ref_blob,
+                            tier_type: 'sequence'
+                        }]
+                    });
+                }
+
+                oReq.send();
+
+
             })
         }
     }
